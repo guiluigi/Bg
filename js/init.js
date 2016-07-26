0658b0134023c64aefc19449e0885f783c065855
Luigieiro <guilherme_luigi17@hotmail.com>
2016-07-26 19:25:25 -0300

    Base do slideshow pronta, adicionado novas fontes

--- js/init.js
+++ js/init.js
@@ -4,11 +4,27 @@
   $(function(){
     $('.parallax').parallax();
     $(".dropdown-button").dropdown();
-    $('.slider').slider({full_width: true});
-    $('.carousel').carousel();
+    $('.slider').slider();
+    $('.carousel').carousel({full_width: true});
     $(".button-collapse").sideNav('show');
     $('.collapsible').collapsible();
     $(".button-collapse").sideNav();
+    
+    $('#btnPrev').click(function(){
+				$('.slider').slider('prev');
+		});
+		$('#btnNext').click(function(){
+				$('.slider').slider('next');
+		});
+
+    $('.slider').mouseenter(function(e) {
+        $('.slider').slider('pause');
+    });
+
+    $('.slider').mouseleave(function(e) {
+        $('.slider').slider('start');
+    });
+
   });
     // end of document ready
 })(jQuery); // end of jQuery name space
